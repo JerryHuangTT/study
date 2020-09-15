5e6eabe26ba79828f3130da169af57529e6329a9
JerryHuangTT <Jerry.Huang@pentair.com>
2020-09-15 06:36:23 +0000

    14:36

--- tf2/classification/0915/main.py
+++ tf2/classification/0915/main.py
@@ -44,11 +44,11 @@ def parse_data():
 
 (x_train, y_train), (x_test, y_test) = parse_data()
 
-input_x = tf.keras.Input(shape=(num_feature),name='input_x')
-h1_l = tf.keras.layers.Dense(64, activation=tf.nn.relu,name='h1_1')(input_x)
-h2_l = tf.keras.layers.Dense(32, activation=tf.nn.relu,name='h2_1')(h1_l)
-h3_l = tf.keras.layers.Dense(16, activation=tf.nn.relu,name='h3_1')(h2_l)
-output_y = tf.keras.layers.Dense(num_class, activation=tf.nn.softmax,name='output_y')(h3_l)
+input_x = tf.keras.Input(shape=(num_feature))
+h1_l = tf.keras.layers.Dense(64, activation=tf.nn.relu)(input_x)
+h2_l = tf.keras.layers.Dense(32, activation=tf.nn.relu)(h1_l)
+h3_l = tf.keras.layers.Dense(16, activation=tf.nn.relu)(h2_l)
+output_y = tf.keras.layers.Dense(num_class, activation=tf.nn.softmax)(h3_l)
 model = tf.keras.Model(inputs=input_x,outputs=output_y)
 
 model.compile(optimizer=tf.train.AdamOptimizer(1e-3),
@@ -57,4 +57,4 @@ model.compile(optimizer=tf.train.AdamOptimizer(1e-3),
 model.fit(x=x_train,y=y_train,
             batch_size=128,epochs=50)
 score = model.evaluate(x_test,y_test)
-model.save('tf2_no_smote_no_normal.h5')
+model.save('tf1_no_smote_no_normal.h5')
--- tf2/classification/0915/main1.py
+++ tf2/classification/0915/main1.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import numpy as np
-new_model = tf.keras.models.load_model('tf2_no_smote_no_normal.h5')
+new_model = tf.keras.models.load_model('tf1_no_smote_no_normal.h5')
 record = np.array([[-0.075197596,0.093203514,0.375011389,-0.017456585,0.85573888,0.925443146],
 [-0.075197596,0.093203514,0.375011389,-0.017456585,0.85573888,0.925443146]])
 res = new_model.predict(record)
