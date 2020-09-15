a3045f77abed7a31df3e8768fcc961c3cf68a5ad
JerryHuangTT <Jerry.Huang@pentair.com>
2020-09-15 13:24:03 +0800

    0915

--- tf2/classification/0824/main.py
+++ tf2/classification/0824/main.py
@@ -37,11 +37,11 @@ def parse_data():
 
 (x_train, y_train), (x_test, y_test) = parse_data()
 
-input_x = tf.keras.Input(shape=(num_feature))
-h1_l = tf.keras.layers.Dense(64, activation=tf.nn.relu)(input_x)
-h2_l = tf.keras.layers.Dense(32, activation=tf.nn.relu)(h1_l)
-h3_l = tf.keras.layers.Dense(16, activation=tf.nn.relu)(h2_l)
-output_y = tf.keras.layers.Dense(num_class, activation=tf.nn.softmax)(h3_l)
+input_x = tf.keras.Input(shape=(num_feature),name='input_x')
+h1_l = tf.keras.layers.Dense(64, activation=tf.nn.relu,name='h1_1')(input_x)
+h2_l = tf.keras.layers.Dense(32, activation=tf.nn.relu,name='h2_1')(h1_l)
+h3_l = tf.keras.layers.Dense(16, activation=tf.nn.relu,name='h3_1')(h2_l)
+output_y = tf.keras.layers.Dense(num_class, activation=tf.nn.softmax,name='output_y')(h3_l)
 model = tf.keras.Model(inputs=input_x,outputs=output_y)
 
 
--- tf2/classification/0915/main.py
+++ tf2/classification/0915/main.py
@@ -1,9 +1,8 @@
 import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
 import numpy as np
-new_model = tf.keras.models.load_model('no_smote_no_normal.h5')
 
-
+new_model = tf.keras.models.load_model('no_smote_no_normal.h5',compile=False)
 record = np.array([[-0.075197596,0.093203514,0.375011389,-0.017456585,0.85573888,0.925443146],
 [-0.075197596,0.093203514,0.375011389,-0.017456585,0.85573888,0.925443146]])
 res = new_model.predict(record)
@@ -11,7 +10,6 @@ rel = res.argmax(axis=1)#tf.argmax(res,axis=-1)
 print(rel)
 
 import pandas as pd
-
 from imblearn.over_sampling import SMOTE
 
 num_class = 0
@@ -47,11 +45,11 @@ def parse_data():
 
 (x_train, y_train), (x_test, y_test) = parse_data()
 
-input_x = tf.keras.Input(shape=(num_feature))
-h1_l = tf.keras.layers.Dense(64, activation=tf.nn.relu)(input_x)
-h2_l = tf.keras.layers.Dense(32, activation=tf.nn.relu)(h1_l)
-h3_l = tf.keras.layers.Dense(16, activation=tf.nn.relu)(h2_l)
-output_y = tf.keras.layers.Dense(num_class, activation=tf.nn.softmax)(h3_l)
+input_x = tf.keras.Input(shape=(num_feature),name='input_x')
+h1_l = tf.keras.layers.Dense(64, activation=tf.nn.relu,name='h1_1')(input_x)
+h2_l = tf.keras.layers.Dense(32, activation=tf.nn.relu,name='h2_1')(h1_l)
+h3_l = tf.keras.layers.Dense(16, activation=tf.nn.relu,name='h3_1')(h2_l)
+output_y = tf.keras.layers.Dense(num_class, activation=tf.nn.softmax,name='output_y')(h3_l)
 model = tf.keras.Model(inputs=input_x,outputs=output_y)
 
 model.compile(optimizer=tf.train.AdamOptimizer(1e-3),
